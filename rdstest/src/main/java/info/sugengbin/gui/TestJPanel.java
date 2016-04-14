a4bce0a2273a49e1796bf9fb839f547ca5b2a6c1
耿槟丶苏 <515474146@qq.com>
2016-04-15 00:04:19 +0800

    add project names

---
+++ rdstest/src/main/java/info/sugengbin/gui/TestJPanel.java
@@ -0,0 +1,16 @@
+package info.sugengbin.gui;
+
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import java.awt.BorderLayout;
+
+public class TestJPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TestJPanel() {
+
+	}
+
+}
